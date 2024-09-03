277ea1479735e2f4e454997b6c49eeb33ffe89f3
Blessan Mathew <blessanm86@gmail.com>
2024-09-03 18:01:11 +0200

    removing unwanted imports

--- src/__tests__/Tracking.test.tsx
+++ src/__tests__/Tracking.test.tsx
@@ -1,5 +1,4 @@
 import { RouterProvider } from '@tanstack/react-router';
-import '@testing-library/jest-dom/vitest';
 import { render, screen } from '@testing-library/react';
 import { userEvent } from '@testing-library/user-event';
 import { expect, test } from 'vitest';
